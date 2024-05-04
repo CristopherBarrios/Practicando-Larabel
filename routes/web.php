b713decdf5afc25cf6e688cd9a091a508ee1c75d
CristopherBarrios <35430376+CristopherBarrios@users.noreply.github.com>
2024-05-03 18:25:19 -0600

    SeAgregaronElModeloDeCAtegoriaYControlador

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\TodosController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,5 @@ Route::get('/tareas/{id}', [TodosController::class, 'show'])->name('todos-edit')
 
 Route::patch('/tareas/{id}', [TodosController::class, 'update'])->name('todos-update');
 Route::delete('/tareas/{id}', [TodosController::class, 'destroy'])->name('todos-destroy');
+
+Route::resource('categories', CategoriesController::class);
